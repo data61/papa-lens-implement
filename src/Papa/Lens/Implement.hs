0abfaa6b5696bd5575d9969f91eb196562dfbd0a
Tony Morris <tmorris@tmorris.net>
2017-01-16 15:28:22 +1000

    Collapse. data61/papa#2

--- src/Papa/Lens/Implement.hs
+++ src/Papa/Lens/Implement.hs
@@ -4,5 +4,6 @@ module Papa.Lens.Implement(
   module P
 ) where
 
+import Papa.Lens.Implement.Data.Collapse as P
 import Papa.Lens.Implement.Data.List as P
 import Papa.Lens.Implement.Data.Tuple as P
